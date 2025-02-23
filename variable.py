80b4b1bc67c756f93946c6080a9335b900bfb02b
Faziijutt <juttfazii815@gmail.com>
2025-02-23 03:25:59 -0800

    first commit

---
+++ variable.py
@@ -0,0 +1,11 @@
+a=False
+b="faizan"
+c=7.54
+d=54
+e="False"
+
+a = int(input("enter yoyr float number:"))
+b = int(input("enter your input number:"))
+print("your float number of enter is",a)
+print("your float number of einput is",b)
+print("multiply of a and b:",a/b)
